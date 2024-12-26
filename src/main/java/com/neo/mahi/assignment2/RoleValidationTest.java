9ef6cecab76357578ee71664a3100b30698da345
gouni <gounimahesh9000@gmail.com>
2024-12-26 09:10:49 +0530

    aop

--- src/main/java/com/neo/mahi/assignment2/RoleValidationTest.java
+++ src/main/java/com/neo/mahi/assignment2/RoleValidationTest.java
@@ -4,6 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class RoleValidationTest {
+    //comminting in master
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext("com.neo.mahi.assignment2");
         Manager manager = context.getBean(Manager.class);
